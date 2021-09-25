6a4e439d450f50557f01cf69160e2a6f3b75e650
smilecharm <skkim@changes.co.kr>
2021-09-25 12:30:20 +0900

    2021-0925

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dailysch.create({date:'2021-09-25', counsellor:'김선미', time:'2000-01-01 10:00:00', client:'내담자1'})
+Dailysch.create({date:'2021-09-25', counsellor:'김선미', time:'2000-01-01 14:00:00', client:'내담자2'})
+Dailysch.create({date:'2021-09-25', counsellor:'김선미', time:'2000-01-01 13:00:00', client:'내담자3'})
+Dailysch.create({date:'2021-09-25', counsellor:'박놀이', time:'2000-01-01 10:00:00', client:'내담자4'})
+Dailysch.create({date:'2021-09-25', counsellor:'박놀이', time:'2000-01-01 11:00:00', client:'내담자5'})
+Dailysch.create({date:'2021-09-25', counsellor:'박놀이', time:'2000-01-01 12:00:00', client:'내담자6'})
+Dailysch.create({date:'2021-09-25', counsellor:'이언어', time:'2000-01-01 14:00:00', client:'내담자7'})
+Dailysch.create({date:'2021-09-25', counsellor:'이언어', time:'2000-01-01 15:00:00', client:'내담자8'})
+Dailysch.create({date:'2021-09-25', counsellor:'이언어', time:'2000-01-01 16:00:00', client:'내담자9'})
+Dailysch.create({date:'2021-09-25', counsellor:'이언어', time:'2000-01-01 10:00:00', client:'내담자10'})
