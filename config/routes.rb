be0e89b32115c922951c77c5130e87bc77dd3597
Hyun Ahn <codepawn@icloud.com>
2021-10-04 21:00:12 +0900

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :dailysches
+  resources :dailysches do
+    collection do
+      get :grid
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
